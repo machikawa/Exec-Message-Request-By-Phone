bd181a1072ca75be8e6cc0096e3182a5c48b8e62
Townriver <a1938hm@aiit.ac.jp>
2022-02-18 09:04:15 +0900

    ガワだけ

--- ExecAPI/ViewController.swift
+++ ExecAPI/ViewController.swift
@@ -9,11 +9,53 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    //MARK: - IBOutlets
+    @IBOutlet weak var clientIdTextField: UITextField!
+    @IBOutlet weak var clientSecretTextField: UITextField!
+    @IBOutlet weak var authURLTextFields: UITextField!
+    @IBOutlet weak var restURLTextField: UITextField!
+    @IBOutlet weak var midTextField: UITextField!
+    @IBOutlet weak var resultViewTextField: UITextField!
+    @IBOutlet weak var messageIDTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+
+    //MARK: - IBActions
+    @IBAction func execBtnPressed(_ sender: Any) {
+        if clientIdTextField.text == "" ||
+            clientSecretTextField.text == "" ||
+            authURLTextFields.text == "" ||
+            restURLTextField.text == "" ||
+            midTextField.text == "" ||
+            messageIDTextField.text == ""
+        // 入力規則 NG
+        {
+            resultViewTextField.text = "全項目を入力しないと実行できません"
+            return
+        // 入力OK
+        } else {
+            resultViewTextField.text = "OK"
+        }
+    }
+    
+    private func executeAPIRequest () {
+        
+    }
+    
+    // 削除必須REST
+    //
+    private func dammygetURL () {
+        
+    }
+    
+    private func getURL () {
+        
+    }
 
 }
 
